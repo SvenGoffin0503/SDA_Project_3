3ef180cd63cb4f8127c88eb63f6f1cc5106423e4
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-05-11 02:02:54 +0200

    Update dynamicTimeWarping.c

--- dynamicTimeWarping.c
+++ dynamicTimeWarping.c
@@ -1,8 +1,6 @@
-#include <stddef.h>
 #include <float.h>
 #include <math.h>
 #include "dynamicTimeWarping.h"
-#include "Signal.h"
 
 
 /* ------------------------------------------------------------------------- *
@@ -11,18 +9,21 @@
  *
  * PARAMETERS
  *
- * elSignalA		Element of the signal A
- * elSignalB		Element of the signal B
- * n_coef			Number of coefficient used in the MFCC transformation (it
- *					corresponds to the length of elSignalA and elSignalB)
+ * signal1			First signal
+ * signal2			Second signal
+ * tStep1			The time step of signal1 which must be compared to compute
+ *					the local cost measure
+ * tStep2			The time step of signal2 which must be compared to compute
+ *					the local cost measure
  * ------------------------------------------------------------------------- */
-static int cost (Signal* signA, Signal* signB, size_t tStepA, size_t tStepB){
+static double cost(Signal* signal1, Signal* signal2, size_t tStep1,
+				   size_t tStep2){
 	
 	double absDist = 0;
-	size_t n_coef = signA->n_coef;
+	size_t n_coef = signal1->n_coef;
 	
-	for(size_t i=0; i < signA->n_coef; i++)
-		absDist += fabs(signA->mfcc[i][tStepA] - signB->mfcc[i][tStepB]);
+	for(size_t i=0; i < signal1->n_coef; i++)
+		absDist += fabs(signal1->mfcc[i][tStep1] - signal2->mfcc[i][tStep2]);
 	
 	absDist = (double) absDist/n_coef;
 	return absDist;
@@ -32,18 +33,18 @@ static int cost (Signal* signA, Signal* signB, size_t tStepA, size_t tStepB){
  * This function returns the minimum between the variables a and b of type 
  * size_t.
  * ------------------------------------------------------------------------- */
-static size_t min(size_t a, size_t b){
+static size_t min(int a, int b){
 	
-	return ( a < b ? a : b );
+	return (size_t)( a < b ? a : b );
 }
 
 /* ------------------------------------------------------------------------- *
  * This function returns the maximum between the variables a and b of type
  * size_t.
  * ------------------------------------------------------------------------- */
-static size_t max(size_t a, size_t b){
+static size_t max(int a, int b){
 	
-	return ( a > b ? a : b );
+	return (size_t)( a > b ? a : b );
 }
 
 /* ------------------------------------------------------------------------- *
@@ -64,23 +65,26 @@ double dtw(Signal* signal1, Signal* signal2, size_t locality){
 	
 	size_t height = signal1->size;
 	size_t width = signal2->size;
-	double ACMat[height][width];
+	double ACMat[height + 1][width + 1];
 	
 	// Locality constraint prevents the dtw from computing the score
 	if(locality < abs((int)(height - width)))
 		return DBL_MAX;
 	
-	for(size_t i = 0; i < height; i++)
-		for(size_t j = 0; j < width ; j++)
+	for(size_t i = 0; i <= height; i++)
+		for(size_t j = 0; j <= width ; j++)
 			ACMat[i][j] = DBL_MAX;
 	
 	ACMat[0][0] = 0.0;
 	
 	// Computation of the dtw score
-	for(size_t i = 1; i < height; i++){
-		for (size_t j = max(1, i-locality); j < min(width, i + locality); j++){
+	for(size_t i = 1; i <= height; i++){
+		size_t init = max(1, (int)(i-locality));
+		size_t cond = min((int)width, (int)(i + locality));
+		
+		for (size_t j = init; j <= cond; j++){
 			
-			ACMat[i][j] = cost(signal1, signal2,i, j)
+			ACMat[i][j] = cost(signal1, signal2, i, j)
 						+ minVec(ACMat[i-1][j], ACMat[i][j-1], ACMat[i-1][j-1]);
 		}
 	}
