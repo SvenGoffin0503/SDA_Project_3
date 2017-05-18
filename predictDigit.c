37a2c94d8d0c8720803689799b5bd46f67c469b7
Sven Goffin <Sven.Goffin@student.ulg.ac.be>
2017-05-18 11:27:19 +0200

    Update predictDigit.c

--- predictDigit.c
+++ predictDigit.c
@@ -13,8 +13,8 @@ DigitScore predictDigit(Signal* signal, Database* database, size_t locality) {
 	DigitScore digitScore = {DBL_MAX, 0};
 	
 	for(size_t i = 0; i < 10; i++){
-		LinkedList* curLList = database->samples[i];
-		LLNode* curNode = curLList->head;
+		LLNode* curNode = database->samples[i]->head;
+		
 		
 		while(curNode != NULL){
 			double score = dtw(signal, (Signal*)curNode->value, locality);
